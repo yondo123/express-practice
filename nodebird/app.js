33c1499fd14b32af10577d133377c63a5100754f
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-08-21 18:50:51 +0900

    🚑 static import 오탈자수정

--- nodebird/app.js
+++ nodebird/app.js
@@ -19,7 +19,7 @@ nunjucks.configure('views', {
 });
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'ㅇpublic')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_PRIKEY));
