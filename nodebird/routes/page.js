1fbbb04ba68091912d088bcf04d0027949d8f21b
yondo123 <46988995+yondo123@users.noreply.github.com>
2022-09-13 16:46:48 +0900

    ✨ 회원가입 페이지 라우팅

--- nodebird/routes/page.js
+++ nodebird/routes/page.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    console.log('hi');
-    res.json({
-        message: true
+    const twits = [];
+    res.render('main', {
+        title: '내 정보 - NodeBird',
+        twits,
+        user: req.user
     });
+});
+
+//회원가입
+router.get('/join', (req, res) => {
+    res.render('join', { title: '회원가입 - NodeBird' });
 });
 
 module.exports = router;
